8b9bbc295aa6def4ac3c5021ba563d97200ba427
salom3e <salomemaisuradze2010@gmail.com>
2025-05-07 01:14:59 +0400

    GOA

---
+++ level 06/classwork/classwork2.py
@@ -0,0 +1,18 @@
+# 3) გაასწორეთ მოცემული კოდები:
+
+# 1. first_user = "Lika"
+# 2nduser = "Giorgi"
+# print(first user)
+
+# 2. first name = Data
+# last % name = "random"
+
+# სწორი კოდი
+# 1. 
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
+
+# 2.
+first_name = "Data"
+last_name = "random"
